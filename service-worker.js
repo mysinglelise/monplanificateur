4a90afe8e56608b8344a8d803ad2f57ca3a17b6d
mysinglelise <lisechemla@gmail.com>
2026-03-10 15:57:56 +0100

    Change cache name from v17 to v18
    Updated cache name to version 18.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker pour PWA
-const CACHE_NAME = 'mon-planning-v17';
+const CACHE_NAME = 'mon-planning-v18';
 const BASE_PATH = '/monplanificateur';
 
 self.addEventListener('install', (event) => {
