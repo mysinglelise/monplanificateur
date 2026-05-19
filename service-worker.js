012035106c637c14a0c3714673c6db71d5aa995f
mysinglelise <lisechemla@gmail.com>
2026-05-19 13:26:22 +0200

    Update cache name to mon-planning-v55

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker pour PWA
-const CACHE_NAME = 'mon-planning-v54';
+const CACHE_NAME = 'mon-planning-v55';
 const BASE_PATH = '/monplanificateur';
 
 self.addEventListener('install', (event) => {
